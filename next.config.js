fd434f4402cb87c2333e296998af1a38523cab15
Morita Ayaha <morita@genech.local>
2025-06-25 11:33:57 +0900

    全体を更新・追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
 };
- 
+
+module.exports = nextConfig;
